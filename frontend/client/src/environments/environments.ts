f9664c9b47dd4ebbcc07199f22022c6c491fd6a9
Vinícius Santos <viniciusssantos.pa@gmail.com>
2026-05-23 20:31:05 -0300

    fix enviroment

--- frontend/client/src/environments/environments.ts
+++ frontend/client/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5207'
-    // apiUrl: 'https://dump-software.onrender.com'
+    production: true,
+    // apiUrl: 'http://localhost:5207'
+    apiUrl: 'https://dump-software.onrender.com'
 };
